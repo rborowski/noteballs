824124db9364f05caeb5a3a5aca2363f29167ef3
Rafał Borowski <moderator9781@gmail.com>
2023-11-20 20:10:50 +0100

    Add computed property for characters length

--- src/components/layout/Note.vue
+++ src/components/layout/Note.vue
@@ -3,6 +3,9 @@
     <div class="card-content">
       <div class="content">
         {{ note.content }}
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ noteLength }} characters</small>
+        </div>
       </div>
     </div>
     <footer class="card-footer ">
@@ -13,7 +16,12 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({ note: { type: Object, required: true } })
+
+const noteLength = computed(() => props.note.content.length)
+
 </script>
 
 <style scoped>
