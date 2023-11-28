bb5bb7817b4c198d4e6e1c49aaeefe60175a1a10
Rafał Borowski <moderator9781@gmail.com>
2023-11-28 18:49:35 +0100

    Order notes in descending order (by id field)

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
-import { collection, onSnapshot, doc, setDoc, deleteDoc } from "firebase/firestore";
+import { collection, onSnapshot, doc, setDoc, deleteDoc, query, orderBy } from "firebase/firestore";
 import { db } from "../js/firebase";
 
 const notesCollection = collection(db, "notes")
@@ -9,7 +9,8 @@ export const useNotesStore = defineStore("notes", () => {
   const notes = ref([]);
 
   async function getNotes() {
-    onSnapshot(notesCollection, (querySnapshot) => {
+    const q = query(notesCollection, orderBy("id", "desc"))
+    onSnapshot(q, (querySnapshot) => {
       let dbNotes = [];
       querySnapshot.forEach((doc) => {
         let note = {
@@ -29,13 +30,14 @@ export const useNotesStore = defineStore("notes", () => {
   async function submitNewNote(content) {
     const id = new Date().getTime().toString()
     await setDoc(doc(notesCollection, id), {
+      id,
       content,
     });
   }
 
   async function submitEditNote(id, content) {
     const noteRef = doc(notesCollection, id);
-    await setDoc(noteRef, { content } );
+    await setDoc(noteRef, { content }, { merge: true } );
   }
 
   const getNoteContent = computed(() => (id) =>
