5440e1c4b1547a44c76bc831ce7fd9ee7e3b41d7
Rafał Borowski <moderator9781@gmail.com>
2023-12-02 17:47:20 +0100

    Customize auth page and add button to navbar

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -8,7 +8,7 @@
             Noteballs
           </div>
 
-          <a @click="isMobileMenu = !isMobileMenu" ref="burgerButtonRef" role="button" class="navbar-burger"
+          <a @click.prevent="isMobileMenu = !isMobileMenu" ref="burgerButtonRef" role="button" class="navbar-burger"
             :class="{ 'is-active': isMobileMenu }" aria-label="menu" aria-expanded="false"
             data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
@@ -18,28 +18,32 @@
         </div>
 
         <div id="navbarBasicExample" ref="navbarMenuRef" class="navbar-menu" :class="{ 'is-active': isMobileMenu }">
-
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <RouterLink :to="{ name: 'Notes' }" @click="isMobileMenu = !isMobileMenu" class="navbar-item"
+          <div class="navbar-start">
+            <div class="navbar-item buttons">
+              <button :to="{ name: 'Auth' }" @click="isMobileMenu = !isMobileMenu" class="button is-small is-info"
                 active-class="is-active">
-                Notes
-              </RouterLink>
-              <RouterLink :to="{ name: 'Stats' }" @click="isMobileMenu = !isMobileMenu" class="navbar-item"
-                active-class="is-active">
-                Stats
-              </RouterLink>
-
+                Signup
+              </button>
             </div>
           </div>
+          <div class="navbar-end">
+            <RouterLink :to="{ name: 'Notes' }" @click="isMobileMenu = !isMobileMenu" class="navbar-item"
+              active-class="is-active">
+              Notes
+            </RouterLink>
+            <RouterLink :to="{ name: 'Stats' }" @click="isMobileMenu = !isMobileMenu" class="navbar-item"
+              active-class="is-active">
+              Stats
+            </RouterLink>
+          </div>
         </div>
+
       </div>
     </nav>
   </teleport>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
 import { ref } from "vue"
 import { onClickOutside } from '@vueuse/core'
 
@@ -52,8 +56,11 @@ onClickOutside(navbarMenuRef, () => isMobileMenu.value = false, { ignore: [burge
 </script>
 
 <style>
-.navbar-menu {
-  position: absolute;
-  left: 0;
-  width: 100%;
-}</style>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
--- src/views/ViewAuth.vue
+++ src/views/ViewAuth.vue
@@ -2,37 +2,91 @@
   <div class="auth">
     <div class="tabs is-centered">
       <ul>
-        <li :class="{'is-active' : !register}">
+        <li :class="{ 'is-active': !register }">
           <a @click.prevent="register = false">Login</a>
         </li>
-        <li :class="{'is-active' : register}">
+        <li :class="{ 'is-active': register }">
           <a @click.prevent="register = true">Register</a>
         </li>
       </ul>
     </div>
 
     <div class="card auth-form">
-  <div class="card-content">
-    <div class="title has-text-centered">
-      {{ formTitle }}
+      <div class="card-content">
+        <div class="title has-text-centered">
+          {{ formTitle }}
+        </div>
+        <form
+          @submit.prevent="onSubmit"
+        >
+
+
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input
+                v-model="credentials.email"
+                class="input"
+                placeholder="e.g. alexsmith@gmail.com"
+                type="email"
+                
+              >
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input
+                v-model="credentials.password"
+                class="input"
+                placeholder="Enter a password"
+                type="password"
+              >
+            </div>
+          </div>
+
+          <div class="field is-grouped is-grouped-right">
+            <p class="control">
+              <button class="button is-primary">
+                {{ formTitle }}
+              </button>
+            </p>
+          </div>
+
+        </form>
+      </div>
     </div>
-    <div class="content">
-      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
-    </div>
-  </div>
-</div>
 
 
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 
 const register = ref(false)
+const credentials = reactive({
+  email: "",
+  password: ""
+})
+
+const formTitle = computed(() => register.value ? "Register" : "Login")
+
+function onSubmit() {
+  if (!credentials.email || !credentials.password) {
+   return alert("Please enter e-mail and password")
+  }
+
+  if(!register.value) {
+    return console.log("login")
+  }
+
+  console.log("register")
+}
 
 
-const formTitle = computed(() => register.value ? "Register" : "Login" )
+
+
 
 </script>
 
